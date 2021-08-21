a94848297db9c727fcfd3e5e55d15640a0e678f6
Rupesh Panwar <75510135+rupeshpanwar@users.noreply.github.com>
2021-08-21 09:43:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.get('/',  (req, res) => {
-  res.send('Hello World!');
+app.get('/',function(req,res) {
+  res.send("Hello World");
 });
 
-var server = app.listen(3000,  () => {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-});
+app.listen(3000);
