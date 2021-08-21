98c29541de5d8ae8153b32f67aaf34f63134bef5
Rupesh Panwar <75510135+rupeshpanwar@users.noreply.github.com>
2021-08-21 09:29:27 +0530

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,8 +1,26 @@
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
+ar supertest = require("supertest");
+var should = require("should");
+
+// This agent refers to PORT where the program is running.
+
+var server = supertest.agent("http://localhost:4000");
+
+// UNIT test begin
+
+describe("SAMPLE unit test",function(){
+
+  // #1 should return home page
+  it("should return home page",function(done){
+    // calling home page
+    server
+    .get("/")
+    .expect("Content-type",/text/)
+    .expect(200) // THis is HTTP response
+    .end(function(err,res){
+      // HTTP status should be 200
+      res.status.should.equal(200);
+      done();
     });
   });
+
 });
