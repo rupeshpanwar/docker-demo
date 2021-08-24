686696d5dfcb9d76985343005d1cad0560b9fea0
Rupesh Panwar <75510135+rupeshpanwar@users.noreply.github.com>
2021-08-24 14:37:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res) {
-  res.send("welcome to Docker-Jenkins");
+  res.send("welcome to Docker-Jenkins trail");
 });
 
 app.listen(3000);
